62370c5724d4c5f22fc58d65faa3baff6ed4d913
Ruanito Santos <ruanitodiego@gmail.com>
2022-07-01 19:07:42 -0300

    Get balance from Stripe API

--- lib/Stripe/Balance/StripeBalanceService.php
+++ lib/Stripe/Balance/StripeBalanceService.php
@@ -2,10 +2,29 @@
 
 namespace Internal\Stripe\Balance;
 
+use Illuminate\Support\Facades\Http;
 use Internal\Bank\BalanceResponse;
 
 class StripeBalanceService {
+    private string $key;
+    private string $url;
+
+    public function __construct() {
+        $this->key = env('STRIPE_PRIVATE_KEY');
+        $this->url = env('STRIPE_URL');
+    }
+    /**
+     * @throws \Exception
+     *
+     * @return BalanceResponse
+     */
     public function getBalance(): BalanceResponse {
-        return new BalanceResponse(1000, 'usd');
+        $balance = Http::withToken($this->key)->get("{$this->url}/balance");
+
+        if ($balance) {
+            return new BalanceResponse($balance['available'][0]['amount'], $balance['available'][0]['currency']);
+        }
+
+        throw new \Exception('StripBalanceServiceError');
     }
 }
