8a230f6a770ab67066a4b901ff8c3627d5db4c3d
Ruanito Santos <ruanitodiego@gmail.com>
2022-07-09 19:07:08 -0300

    Create parameter validation for payment

--- app/Http/Controllers/PaymentsController.php
+++ app/Http/Controllers/PaymentsController.php
@@ -2,11 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use Internal\Stripe\Payment\StripePaymentService;
 
 class PaymentsController extends Controller {
-    public function create(): RedirectResponse {
+    public function create(Request $request): mixed {
+        $validator = Validator::make($request->all(), [
+            'payment' => 'required',
+            'payment.*.price' => 'required',
+            'payment.*.quantity' => 'required|numeric',
+        ]);
+
+        if ($validator->fails()) {
+            return response()
+                ->json(['status' => 'error', 'message' => $validator->getMessageBag()], 400);
+        }
+
         $payment = (new StripePaymentService())->createPayment();
         return redirect()->away($payment->getRedirectUrl());
     }
--- tests/Feature/app/Http/Controllers/PaymentsControllerTest.php
+++ tests/Feature/app/Http/Controllers/PaymentsControllerTest.php
@@ -14,11 +14,30 @@ class PaymentsControllerTest extends TestCase {
         ]);
     }
 
-    public function test_createPayment() {
+    public function test_shouldCreatePayment() {
         $this->mockSuccessRequest();
         $expectedStatusCode = 302;
-        $response = $this->call('POST', 'api/payments');
+        $response = $this->call('POST', 'api/payments', [
+            'payment' => [
+                ['price' => 'price', 'quantity' => 1],
+            ],
+        ]);
 
         $response->assertStatus($expectedStatusCode);
+    }
+
+    public function test_shouldNotCreateWithoutParameters() {
+        $this->mockSuccessRequest();
+        $expectedStatusCode = 400;
+        $response = $this->call('POST', 'api/payments', ['payment' => [[]]]);
+
+        $response->assertStatus($expectedStatusCode);
+        $response->assertJson([
+            'status' => 'error',
+            'message' => [
+                'payment.0.price' => ['The payment.0.price field is required.'],
+                'payment.0.quantity' => ['The payment.0.quantity field is required.'],
+            ],
+        ]);
     }
 }
